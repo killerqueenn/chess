5a33d431fafb2b2bfb575ca8ad50241d169338da
Ekaterina <askaterina03@mail.ru>
2020-09-26 13:00:36 +0500

    написала прогу))

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int king=1, queen=2, rook=3, knight=4, bishop=5, pawn=6;
+            int piece;
+
+            Console.WriteLine("Введите название фигуры: ");
+            piece = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите начальное положение фигуры: ");
+            string nachalo = Console.ReadLine();
+
+            Console.WriteLine("Введите конечное положение фигуры: ");
+            string konechn = Console.ReadLine();
+
+            if (piece == knight)
+            {
+                if (knight1.IsCorrectKnight(nachalo[0], nachalo[1], konechn[0], konechn[1]))
+                {
+                    Console.WriteLine("верно");
+                }
+                else
+                {
+                    Console.WriteLine("неверно");
+                }
+            }
+        }
+    }
+}
---
+++ knight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    class knight1
+    {
+        public static bool IsCorrectKnight(int x, int y, int x1, int y1)
+        {
+            bool state;
+            if (((x1 == x + 1) || (x1 == x - 1)) && ((y1 == y + 2) || (y1 == y - 2)) || ((x1 == x + 2) || (x1 == x - 2)) && ((y1 == y + 1) || (y1 == y - 1)))
+            {
+                state = true;
+            }
+            else
+            {
+                state = false;
+            }
+            return state;
+        }
+    }
+}
